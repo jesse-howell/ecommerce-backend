1c63a2432868a130d57afea9fdb6d94391fdf9ad
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-03 21:16:54 -0500

    added category model requirements

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,47 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+Category.init({
+    shirts: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    category_name: 'Shirts',
+      allowNull: false,
+    },
+    shorts: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    category_name: 'Shorts',
+      allowNull: false,
+    },
+    shoes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    category_name: 'Shoes',
+      allowNull: false,
+    },
+    hats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    category_name: 'Hats',
+      allowNull: false,
+    },
+    music: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    category_name: 'Music',
+      allowNull: false,
+    },
   },
   {
     sequelize,
