35cf4d502fe7605fef493e6630e23d2bf9f67776
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-03 22:52:10 -0500

    updated product model

--- models/Product.js
+++ models/Product.js
@@ -2,13 +2,13 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
+Product.init({
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -18,6 +18,24 @@ Product.init(
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+     references: { 
+      model: Category,
+      key: 'id',
+      } 
     },
   },
   {
