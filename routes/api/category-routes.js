6e029889654b5998592fd398bebaafbf0c1b8a76
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-04 22:37:23 -0500

    updated '/' route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,32 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { SELECT } = require("sequelize/types/lib/query-types");
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const categories = await Category.findAll();
+  console.log(categories.every((category) => category instanceof Category));
+  console.log("All categories:", JSON.stringify(categories, null, 2));
+
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
 });
 
