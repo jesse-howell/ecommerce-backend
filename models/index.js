201a50528b19af1be9eab740d5306879598e1324
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-11 01:17:57 -0400

    added ./types/lib/query-types to exports in node_modules/sequelize/package.json

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ Category.hasMany(Product, {
   onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { through: ProductTag });
 
