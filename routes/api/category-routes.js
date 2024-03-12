e17b56c233ba4df382af71ac3da446e2b8a5e36e
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2024-03-11 22:08:34 -0400

    removed select from category-routes, reset mysql server

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { SELECT } = require("sequelize/types/query-types");
 const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
